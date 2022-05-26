639acc6b7a9242b64c6b77381f3edbc14334c2a1
Varga Krisztián <vargakrisztian2004@gmail.com>
2022-05-26 17:18:14 +0200

    Bábuk combobox kész

--- Sakklepesek/Sakklepesek/MainWindow.xaml.cs
+++ Sakklepesek/Sakklepesek/MainWindow.xaml.cs
@@ -23,6 +23,23 @@ namespace Sakklepesek
         public MainWindow()
         {
             InitializeComponent();
+            Felulet();
         }
+
+        private void Felulet()
+
+        {
+            ComboBox babuk = new ComboBox();
+            babuk.Items.Add("Király");
+            babuk.Items.Add("Királynő");
+            babuk.Items.Add("Bástya");
+            babuk.Items.Add("Futó");
+            babuk.Items.Add("Huszár");
+            babuk.Items.Add("Világos gyalog");
+            babuk.Items.Add("Sötét gyalog");
+            ablak.Children.Add(babuk);
+            babuk.Height = 25;
+        }
+
     }
 }
